636afd43a7f01b07586c48954cc4d4354c507644
Aer0m <47530448+Aer0m@users.noreply.github.com>
2023-04-20 13:46:07 +0300

    download_excel

--- src/main/java/app/ApachePOI/Download.java
+++ src/main/java/app/ApachePOI/Download.java
@@ -1,5 +1,6 @@
 package app.ApachePOI;
 
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -11,6 +12,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.sql.*;
+import java.util.ArrayList;
+import database.config.*;
 
 @WebServlet("/download")
 public class Download extends HttpServlet {
@@ -28,10 +32,35 @@ public class Download extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html");
         String sort = request.getParameter("sort");
-        String sql = "SELECT * FROM users";
-        XSSFWorkbook workbook = new XSSFWorkbook();
-        XSSFSheet sheet = workbook.createSheet("список_сотрудников");
+        ArrayList<String> employers = new ArrayList<>();
 
+        try {
+            try (Connection connection = DriverManager.getConnection(DbConfig.getUrl(), DbConfig.getUser(), DbConfig.getPassword())) {
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery("SELECT * FROM employers ORDER BY fullname");
+                while(resultSet.next()){
+                    employers.add(resultSet.getString(2));
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        XSSFWorkbook workbook = new XSSFWorkbook();
+        XSSFSheet sheet = workbook.createSheet("List1");
+        int rownum = 0;
+        for (int i = 0; i < employers.size(); i++) {
+            Row row = sheet.createRow(rownum++);
+            row.createCell(0).setCellValue(employers.get(i));
+        }
+
+        response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+        response.setHeader("Content-Disposition", "attachment; filename=employers.xlsx");
+        workbook.write(response.getOutputStream());
     }
 }
